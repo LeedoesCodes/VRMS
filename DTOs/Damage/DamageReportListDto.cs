c5efe858afc0ca38bbb64862d548e0079d25cf34
lee <leesingeon@gmail.com>
2026-01-16 05:02:43 +0800

    fix dto

---
+++ DTOs/Damage/DamageReportListDto.cs
@@ -0,0 +1,6 @@
+﻿public class DamageReportListDto
+{
+    public int DamageReportId { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal EstimatedCost { get; set; }
+}
